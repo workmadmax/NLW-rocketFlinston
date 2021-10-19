f638172991cc6ae7cf46ebc98b5d5bf384227141
madmax42 <work.madmax@gmail.com>
2021-10-19 12:22:14 -0300

    pause

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,9 +1,34 @@
+import axios from "axios";
+import { response } from "express";
 
+interface IAccessTokenResponse {
+  access_token: string;
+}
 
 class AuthenticateUserService {
-    async execute(code: string) {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
 
-    }
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENTE_ID,
+          cliente_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          "Accept": "application/json",
+        },
+      });
+
+    const response = await axios.get("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`,
+      },
+    });
+
+    return response.data;
+  }
 }
 
 export { AuthenticateUserService };
